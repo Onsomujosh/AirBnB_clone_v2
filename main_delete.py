360e574002d60f8cdcd970b559633edcacdeaa76
DevWambugu <venusdata100@gmail.com>
2023-09-20 23:24:48 +0000

    add documentation

--- main_delete.py
+++ main_delete.py
@@ -32,7 +32,7 @@ fs.new(another_state)
 fs.save()
 print("Another State: {}".format(another_state))
 
-# All States
+# All States:
 all_states = fs.all(State)
 print("All States: {}".format(len(all_states.keys())))
 for state_key in all_states.keys():
