7484d3eb898bc6170d6a9fcb67362cff86b14be3
DevWambugu <venusdata100@gmail.com>
2023-09-24 00:29:57 +0000

    solve inline comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is None:
-            return self.__objects #Return the entire dictionary
+            return self.__objects  # Return the entire dictionary
         cls_name =cls.__name__
         dct = {}
         for key in self.__objects.keys():
