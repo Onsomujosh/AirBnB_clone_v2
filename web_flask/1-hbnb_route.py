e3bb88ef173195f4ac32f25782829db10632239c
Onsomujosh <Joshuaonsomu@gmail.com>
2023-10-20 12:39:56 -0700

    hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,7 +15,7 @@ def hello():
     return "Hello HBNB!"
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Function that returns on url /hbnb
